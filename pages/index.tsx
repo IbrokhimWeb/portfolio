8c0d854d18f90401225b24b47743de5b32dd989c
IbrokhimWeb <ibrokhimweb@gmail.com>
2023-01-18 23:09:37 +0500

    Social MEdia added

--- pages/index.tsx
+++ pages/index.tsx
@@ -35,21 +35,21 @@ const Home: NextPage = () => {
                         </p>
                         <div className="flex items-center gap-5 mt-8">
                             <Link href="/about" className={`${s.button} w-[8em] h-[7vh] flex items-center justify-center bg-gray-900 hover:bg-gray-800 text-[1.2rem] text-white`}>About Me</Link>
-                            <a className={`${s.button} cursor-pointer w-[8em] h-[7vh] flex items-center font-semibold justify-center bg-green-500 hover:bg-green-600 hover:text-white text-[1.2rem]`}>Download SV</a>
+                            <Link href="" className={`${s.button} cursor-pointer w-[8em] h-[7vh] flex items-center font-semibold justify-center bg-green-500 hover:bg-green-600 hover:text-white text-[1.2rem]`}>Download SV</Link>
                         </div>
                         <div className="flex items-center gap-6 mt-8">
-                            <a target="_black" href="https://www.youtube.com/cyber_step1">
+                            <Link target="_black" href="https://www.youtube.com/cyber_step1">
                                 <BsYoutube className="text-white text-[2rem] hover:text-green-500" />
-                            </a>
-                            <a target="_black" href="https://github.com/IbrokhimWeb">
+                            </Link>
+                            <Link target="_black" href="https://github.com/IbrokhimWeb">
                                 <BsGithub className="text-white text-[2rem] hover:text-green-500" />
-                            </a>
-                            <a target="_black" href="https://www.instagram.com/ibrokhimweb/">
+                            </Link>
+                            <Link target="_black" href="https://www.instagram.com/ibrokhimweb/">
                                 <FiInstagram className="text-white text-[2rem] hover:text-green-500" />
-                            </a>
-                            <a target="_black" href="https://t.me/s/cyber_step">
+                            </Link>
+                            <Link target="_black" href="https://t.me/s/cyber_step">
                                 <FaTelegramPlane className="text-white text-[2rem] hover:text-green-500" />
-                            </a>
+                            </Link>
                         </div>
                     </section>
                     <section>
